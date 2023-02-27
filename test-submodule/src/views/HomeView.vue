99775d707c5739cbfeb46c36e77ab51738a6ec0b
soaresrysqer <soares@rysqer.com>
2023-02-27 13:24:06 +0100

    submodule

---
+++ test-submodule/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import TestComponent from '../../../real-world-vue/src/components/TestComponent.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <TestComponent />
+  </main>
+</template>
